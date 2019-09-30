ef3a16207d43737674678f18200c2e61f9a6bd6e
Eduardo <edunb01@gmail.com>
2019-09-30 13:53:02 -0300

    aula 4

---
+++ dia3/sugestaodadosAluno.R
@@ -0,0 +1,6 @@
+Programa Nacional de Alimentação Escolar
+ftp://ftp2.fnde.gov.br/dadosabertos/PNAE/PNAE_RECURSOS_REPASSADOS_2018.csv
+
+Dicionário de Dados
+ftp://ftp2.fnde.gov.br/dadosabertos/PNAE/Dicion%C3%A1rio%20de%20Dados_Alunos_Escolas_Financeiro.pdf
+
